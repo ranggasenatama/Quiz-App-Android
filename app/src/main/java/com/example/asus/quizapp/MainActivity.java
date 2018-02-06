777cffad8ff77e3596f544e5f251a4bc16d6c1ff
Rangga <ranggasenatama@gmail.com>
2018-02-06 21:40:06 +0700

    set START Quiz layout + logic

--- app/src/main/java/com/example/asus/quizapp/MainActivity.java
+++ app/src/main/java/com/example/asus/quizapp/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.asus.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    public void clickStartQuiz(View view){
+        button.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.startQuizButton);
     }
 }
