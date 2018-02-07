90a1b7dab835a9c38762acba989ff6cb4155d49a
Rangga <ranggasenatama@gmail.com>
2018-02-07 18:00:35 +0700

    true false answer

--- app/src/main/java/com/example/asus/quizapp/MainActivity.java
+++ app/src/main/java/com/example/asus/quizapp/MainActivity.java
@@ -13,20 +13,20 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     List<Integer> listAnswer = new ArrayList<Integer>();
+    int shuffABC;
+    Button button;
+    TextView answerTextView;
 
     public void clickAnswer(View view) {
-        if (view.getId() == R.id.button1) {
-
-        } else if (view.getId() == R.id.button2) {
-
-        } else if (view.getId() == R.id.button3) {
-
+        if (view.getTag().toString().equals(Integer.toString(shuffABC + 1))) {
+            answerTextView.setText("CORRECT!!");
+            answerTextView.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
         } else {
-
+            answerTextView.setText("INCORECT!!");
+            answerTextView.setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
         }
+        answerTextView.setVisibility(View.VISIBLE);
     }
-
-    Button button;
 
     public void clickStartQuiz(View view) {
         button.setVisibility(View.INVISIBLE);
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.startQuizButton);
+        answerTextView = findViewById(R.id.answerTextView);
         Button answerButton1 = (Button) findViewById(R.id.button1);
         Button answerButton2 = (Button) findViewById(R.id.button2);
         Button answerButton3 = (Button) findViewById(R.id.button3);
@@ -45,22 +46,21 @@ public class MainActivity extends AppCompatActivity {
         TextView sumTextView = (TextView) findViewById(R.id.sumTextView);
 
         Random random = new Random();
+        answerTextView.setVisibility(View.INVISIBLE);
 
         int nilai1 = random.nextInt(50);
         int nilai2 = random.nextInt(50);
 
-        sumTextView.setText(nilai1 + " + "+ nilai2);
+        sumTextView.setText(nilai1 + " + " + nilai2);
 
+        shuffABC = random.nextInt(3);
 
-        int shuffABC = random.nextInt(3);
-
-        for(int i=0;i<4;i++){
+        for (int i = 0; i < 4; i++) {
             int acak = random.nextInt(100);
-            if(shuffABC==i){
-                listAnswer.add(nilai1+nilai2);
-            }
-            else{
-                while(acak==nilai1+nilai2){
+            if (shuffABC == i) {
+                listAnswer.add(nilai1 + nilai2);
+            } else {
+                while (acak == nilai1 + nilai2) {
                     acak = random.nextInt(100);
                 }
                 listAnswer.add(acak);
